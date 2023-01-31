cc9ab321ee80a7075f037f3378d4e1a59170cac5
Federico Cáceres <federicoenmanuel@gmail.com>
2023-01-30 21:53:04 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+  state = {
+    firstName:'',
+    lastName: '',
+    userList: []
+  }
+
+  changeFirstName = (e) => {
+    this.setState({
+      firstName: e.target.value
+    })
+  }
+
+  changeLastName = (e) => {
+    this.setState({
+      lastName: e.target.value
+    })
+  }
+
+  insertUser = (e) =>{
+    e.preventDefault();
+    this.setState({
+      userList: [...this.state.userList, {'firstName':this.state.firstName, 'lastName':this.state.lastName}],
+      firstName:'',
+      lastName: ''
+    })
+  }
+
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
-            <form>
+            <form onSubmit={ this.insertUser }>
               <div className="form-group">
                 <label htmlFor="first-name">Nombre</label>
-                <input type="text" className="form-control" name="first-name" />
+                <input type="text" className="form-control" name="first-name" value={this.state.firstName} onChange={this.changeFirstName} />
               </div>
 
               <div className="form-group">
                 <label htmlFor="last-name">Apellido</label>
-                <input type="text" className="form-control" name="last-name" />
+                <input type="text" className="form-control" name="last-name" value={this.state.lastName} onChange={this.changeLastName}/>
               </div>
 
               <div className="action">
-                <button type="submit" className="btn btn-primary">Agregar Invitado</button>
+                <button type="submit" className="btn btn-primary" onClick={this.insertUser}>Agregar Invitado</button>
               </div>
             </form>
 
@@ -30,7 +58,14 @@ class App extends Component {
                 </tr>
               </thead>
               <tbody>
-
+              {
+                this.state.userList.map( (el, i) =>{
+                  return  <tr key={i}>
+                            <td>{el.firstName}</td>
+                            <td>{el.lastName}</td>
+                          </tr>
+                })
+              }
               </tbody>
             </table>
           </div>
